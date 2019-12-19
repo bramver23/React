8113107579a6fd38bdfb82e2cdff004ba959ab53
bramver23 <bramver23@hotmail.com>
2019-12-19 17:29:24 -0500

    test

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class navbar extends Component {
     render() { 
         return ( <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar  Testing changes</a>
+        <a className="navbar-brand" href="#">Navbar  xxTesting changes</a>
       </nav> );
     }
 }
