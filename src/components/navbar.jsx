2437c60170f34b60e9620b6f142976307f516cef
bramver23 <bramver23@hotmail.com>
2019-12-19 20:54:27 -0500

    test update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class navbar extends Component {
     render() { 
         return ( <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar x MENU</a>
+        <a className="navbar-brand" href="#">Navbar xss MENU</a>
       </nav> );
     }
 }
